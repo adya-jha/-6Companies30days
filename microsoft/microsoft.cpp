768ba4d7f1d831b1d6ebecaf68c74154366f2297
adyajha07 <154891585+adyajha07@users.noreply.github.com>
2025-01-04 22:05:03 +0530

    Updated the cpp file with [describe your changes]

--- microsoft/microsoft.cpp
+++ microsoft/microsoft.cpp
@@ -50,6 +50,35 @@ int incremovableSubarrayCount(vector<int>& nums) {
         return result;
     }
 
+//question4 - image smoother 
+vector<vector<int>> directions {
+{-1,-1},{-1,0},{-1,1},
+{0,-1},{0,0},{0,1},
+{1,-1},{1,0},{1,1}
+};
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        int m=img.size();
+        int n=img[0].size();
+        vector<vector<int>> results(m,vector<int>(n,0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int sum=0;
+                int count=0;
+                for(auto &it:directions){
+                int i_=i+it[0];
+                int j_=j+it[1];
+                if(i_>=0 && i_<m && j_>=0 && j_<n){
+                    sum+=img[i_][j_];
+                    count++;
+                }
+                }
+                results[i][j]=sum/count;
+                }
+            }
+            return results;
+        }
+
+//question 5- 
 
 int main(){
     cout<<"solving question for company one";
